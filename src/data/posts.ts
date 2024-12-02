47ab5c0d6a22484d6993d244111e8cb854266d1a
JunyaUkyou <ukyou.junya0514@gmail.com>
2024-12-02 21:42:23 +0900

    move content public folter

--- src/data/posts.ts
+++ src/data/posts.ts
@@ -4,7 +4,7 @@ export const posts = [
     title: "（編集中）姿勢推定でドラゴンボールの技をだす",
     summary:
       "姿勢推定AIを使用して、スーパーサイヤ人になったり、かめはめ波を打つ方法を解説します",
-    content: "/src/content/post1.html",
+    content: "/content/post1.html",
     createdAt: "2024-12-02",
     updatedAt: "2024-12-02",
     repository: "https://github.com/JunyaUkyou/dragonball-effects",
